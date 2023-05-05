7132ae7e26e7d7088e6c673a5662c6072e805021
DavidTorrejano <Davitorrejano@gmail.com>
2023-05-05 05:16:09 -0500

    Aprendiendo

---
+++ src/com/interfaces/Cadenas.java
@@ -0,0 +1,16 @@
+package com.interfaces;
+
+public class Cadenas {
+
+    public static void main(String[] args) {
+
+        String cadena= "Esto es una oración";
+
+        for(int i=0; i<cadena.length(); i++){
+            System.out.print(cadena.charAt(i));
+        }
+        
+
+    }
+
+}
--- src/com/interfaces/main.java
+++ src/com/interfaces/main.java
@@ -3,12 +3,33 @@ package com.interfaces;
 public class main {
 
     public static void main(String[] args) {
-        CocheCRUD cocheCRUD= new CocheCRUDImpl();
+        //5:56
+
+       /* CocheCRUD cocheCRUD= new CocheCRUDImpl();
 
         cocheCRUD.save();
         cocheCRUD.findAll();
         cocheCRUD.delete();
 
         System.out.println("Prueba v2");
+
+        */
+
+        for(int i=5; i>0; i--){
+            for(int j=i; j>0; j--){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int i=3; i>=-3; i--){
+            for(int j= Math.abs(i); j<4; j++){
+                System.out.print('#');
+            }
+            System.out.println();
+        }
+
     }
 }
