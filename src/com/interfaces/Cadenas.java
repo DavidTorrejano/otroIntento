1974537df5c6dc822772eba099de7b53cd10db42
DavidTorrejano <Davitorrejano@gmail.com>
2023-05-05 14:37:36 -0500

    más practica

--- src/com/interfaces/Cadenas.java
+++ src/com/interfaces/Cadenas.java
@@ -1,15 +1,37 @@
 package com.interfaces;
 
+import java.util.Vector;
+
 public class Cadenas {
 
     public static void main(String[] args) {
 
-        String cadena= "Esto es una oración";
+        Vector <Integer> num= new Vector<>();
 
-        for(int i=0; i<cadena.length(); i++){
-            System.out.print(cadena.charAt(i));
+        num.add(3);
+        num.add(2);
+        num.add(1);
+        num.add(0);
+        num.add(-1);
+
+        for(int i: num){
+            System.out.println(i);
         }
-        
+
+        System.out.println("______________________");
+
+        for(int i=0; i<num.size(); i++){
+            System.out.println(num.get(i));
+        }
+
+        System.out.println("______________________");
+
+        for(int i=num.size()-1; i>=0; i--){
+            System.out.println(num.get(i));
+        }
+
+
+
 
     }
 
